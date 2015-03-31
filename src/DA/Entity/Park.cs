7ed864ddf9caacd574c9cc5cf73f78fb576ff698
hyperzhang <huo-hyper.zhang@schneider-electric.com>
2015-03-31 13:42:49 +0800

    add park
    Signed-off-by: hyperzhang <huo-hyper.zhang@schneider-electric.com>

---
+++ src/DA/Entity/Park.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SE.DSP.Pop.Entity
+{
+    public class Park
+    {
+        public Park()
+        {
+        }
+
+        public long HierarchyId { get; set; }
+
+        public decimal FloorSpace { get; set; }
+
+        public decimal BuildArea { get; set; }
+    }
+}
---
+++ src/Script/DB/0.3/03CreatePark.sql
@@ -0,0 +1,22 @@
+/****** Object:  Table [dbo].[Gateway]    Script Date: 03/31/2015 10:33:50 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Park]') AND type in (N'U'))
+BEGIN
+	CREATE TABLE [dbo].[Park](
+		[HierarchyId] [bigint] NOT NULL,
+		[FloorSpace] decimal(19,2) NOT NULL,
+		[BuildingArea] decimal(19,2) NOT NULL,
+	 CONSTRAINT [PK_Park] PRIMARY KEY CLUSTERED 
+	(
+		[HierarchyId] ASC
+	)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+	) ON [PRIMARY]
+
+END
+
+
