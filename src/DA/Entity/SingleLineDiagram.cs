fe7c8c4c691e4218db2c43f67dd84acae4853350
hyperzhang <huo-hyper.zhang@schneider-electric.com>
2015-04-03 15:32:22 +0800

    add SingleLineDiagram repository
    Signed-off-by: hyperzhang <huo-hyper.zhang@schneider-electric.com>

---
+++ src/DA/Entity/SingleLineDiagram.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SE.DSP.Pop.Entity
+{
+    public class SingleLineDiagram
+    {
+        public long Id { get; set; }
+
+        public long HierarchyId { get; set; }
+
+        public string Key { get; set; }
+
+        public int Order { get; set; }
+
+        public DateTime CreateTime { get; set; }
+
+        public string CreateUser { get; set; }
+
+        public DateTime? UpdateTime { get; set; }
+
+        public string UpdateUser { get; set; }
+    }
+}
--- src/Script/DB/0.3/06CreateSingleLineDiagram.sql
+++ src/Script/DB/0.3/06CreateSingleLineDiagram.sql
@@ -8,12 +8,17 @@ GO
 IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[SingleLineDiagram]') AND type in (N'U'))
 BEGIN
 	CREATE TABLE [dbo].[SingleLineDiagram](
+	    [Id] [bigint] identity(1,1) NOT NULL,
 		[HierarchyId] [bigint] NOT NULL,
-		[BuildingArea] decimal(19,2) NOT NULL,
-		[FinishingDate] datetime NOT NULL
-	 CONSTRAINT [PK_Building] PRIMARY KEY CLUSTERED 
+		[Key] nvarchar(100) NOT NULL, 
+		[Order] int NOT NULL,
+		[CreateTime] datetime NOT NULL,
+		[CreateUser] nvarchar(100) NOT NULL,
+		[UpdateTime] datetime NOT NULL,
+		[UpdateUser] nvarchar(100) NOT NULL
+	 CONSTRAINT [PK_SingleLineDiagram] PRIMARY KEY CLUSTERED 
 	(
-		[BuildingId] ASC
+		[Id] ASC
 	)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
 	) ON [PRIMARY]
 
