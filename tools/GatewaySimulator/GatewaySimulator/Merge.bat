861229f8d742dfdf0c8f2170b6b86d8d8d6d0630
Aries Zhang <feng-aries.zhang@schneider-electric.com>
2015-04-09 16:02:03 +0800

    added default hierarchy data

---
+++ tools/GatewaySimulator/GatewaySimulator/Merge.bat
@@ -0,0 +1,4 @@
+﻿@echo off
+
+ilmerge /target:winexe /targetplatform:v4,C:\Windows\Microsoft.NET\Framework64\v4.0.30319 /out:Simulator.exe GatewaySimulator.exe M2Mqtt.dll Newtonsoft.Json.dll /log:"merge.log"
+copy /Y GatewaySimulator.exe.config Simulator.exe.config
