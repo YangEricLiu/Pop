a91f1bc5b002a992f5dea6059e746e8d65864766
hyperzhang <huo-hyper.zhang@schneider-electric.com>
2015-04-16 11:09:34 +0800

    add new controller for scene log and pictures
    Signed-off-by: hyperzhang <huo-hyper.zhang@schneider-electric.com>

---
+++ src/Web/WebHost/Controllers/SceneLogController.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System.Web.Http;
+using SE.DSP.Foundation.Web.Wcf;
+using SE.DSP.Pop.BL.API;
+using SE.DSP.Pop.BL.API.DataContract;
+using SE.DSP.Pop.Web.WebHost.Model;
+
+namespace SE.DSP.Pop.Web.WebHost.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/scenelog")]
+    public class SceneLogController : ApiController
+    {
+        private readonly IPopClientService clientService;
+
+        public SceneLogController()
+        {
+            this.clientService = ServiceProxy<IPopClientService>.GetClient("IPopClientService.EndPoint");
+        }
+
+        [HttpGet]
+        [Route("upload/{hierarchyId}")]
+        public SceneLogModel[] Upload(long hierarchyId, [FromBody]SceneLogModel[] logs)
+        {
+            var dto = logs.Select(s => AutoMapper.Mapper.Map<SceneLogDto>(s)).ToArray();
+
+            var result = this.clientService.UploadSceneLog(hierarchyId, dto);
+
+            return result.Select(s => AutoMapper.Mapper.Map<SceneLogModel>(s)).ToArray();
+        }
+    }
+}
---
+++ src/Web/WebHost/Controllers/ScenePictureController.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System.Web.Http;
+using SE.DSP.Foundation.Web.Wcf;
+using SE.DSP.Pop.BL.API;
+using SE.DSP.Pop.BL.API.DataContract;
+using SE.DSP.Pop.Web.WebHost.Model;
+
+namespace SE.DSP.Pop.Web.WebHost.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/scenepicture")]
+    public class ScenePictureController : ApiController
+    {
+        private readonly IPopClientService clientService;
+
+        public ScenePictureController()
+        {
+            this.clientService = ServiceProxy<IPopClientService>.GetClient("IPopClientService.EndPoint");
+        }
+
+        [HttpGet]
+        [Route("upload/{hierarchyId}")]
+        public ScenePictureModel[] Upload(long hierarchyId, [FromBody]ScenePictureModel[] logs)
+        {
+            var dto = logs.Select(s => AutoMapper.Mapper.Map<ScenePictureDto>(s)).ToArray();
+
+            var result = this.clientService.UploadScenePicture(hierarchyId, dto);
+
+            return result.Select(s => AutoMapper.Mapper.Map<ScenePictureModel>(s)).ToArray();
+        }
+    }
+}
